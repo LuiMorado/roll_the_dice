54ba0491f1fee26ac8755019f24b85abe526e9ea
LuiMorado <122566152+LuiMorado@users.noreply.github.com>
2024-01-25 13:52:21 +0000

    Update rtd.py
    Added Integer so code knows the user will like between 1 - 6 die to roll with.
    
    Signed-off-by: LuiMorado <122566152+LuiMorado@users.noreply.github.com>

--- rtd.py
+++ rtd.py
@@ -1,3 +1,21 @@
-git init roll_the_dice
-git add VERSION_1
-git commit -m "testing set up successful"
+# rtd.py 
+
+def parse_input(input_string):
+  """Return 'input_string as an integer between 1 and 6.
+
+  check if 'input_string is an integer number between 1 and 6.
+  If so, return an integer with the same value. otherwise, tell the user to enter a valid number and quit the program. """"
+
+  if input_string.strip() in {"1" , "2" , "3" , "4" , "5" , "6"}:
+    return init(input_string)
+  else:
+    print("Pleaseenter a number from 1 to 6.")
+    raise SystemExit(1)
+    
+# Main Code
+# Task 1: Get user's Input, validate it.
+num_dice_input = input("How many die? [-6]")
+num_dice = parse_input(num-dice_input)
+
+
+
