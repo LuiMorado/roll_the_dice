b4b5bb8b00cb8b8b7639a8b935a978cca609d0ba
LuiMorado <122566152+LuiMorado@users.noreply.github.com>
2024-01-29 12:52:42 +0000

    Update rtd.py
    added dice code
    task 1
    
    Signed-off-by: LuiMorado <122566152+LuiMorado@users.noreply.github.com>

--- rtd.py
+++ rtd.py
@@ -14,8 +14,28 @@ def parse_input(input_string):
     
 # Main Code
 # Task 1: Get user's Input, validate it.
-num_dice_input = input("How many die? [-6]")
+num_dice_input = input("How many die? [1-6]")
 num_dice = parse_input(num-dice_input)
 
 
-
+# Dice Code
+# Task 1: import die.
+import random
+min_value = 1
+max_value = 6
+roll_again = "yes"
+While roll_again == "yes" or roll_again == "y":
+  print("Rolling the dices...") 
+  break
+  print("The values are....")
+  break
+  value1 = random.randint (min_value , max_value)
+  value2 = random.randint (min_value , max_value)
+  value3 = random.randint (min_value , max_value)
+  value4 = random.randint (min_value , max_value)
+  value5 = random.randint (min_value , max_value)
+  value6 = random.randint (min_value , max_value)
+  roll_again = input("Type 'Y' or 'Yes' to roll the dices again.")
+  roll_end = input("Type 'N' or 'No' to stop.")
+  break
+print("Yeaaaa boiiii")
